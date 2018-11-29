c416a82d30d404d2caba410771a9f0b058a6c17b
Justin Chan <juchan@uber.com>
2018-11-29 14:31:08 -0500

    Comments

--- src/dbnode/storage/shard.go
+++ src/dbnode/storage/shard.go
@@ -1802,7 +1802,7 @@ func (s *dbShard) Bootstrap(
 		if fs.Status != fileOpNotStarted {
 			continue // Already recorded progress
 		}
-		// TODO(juchan): verify that version 0 is correct here
+
 		s.markFlushStateSuccess(at, 0)
 	}
 
@@ -1951,9 +1951,6 @@ func (s *dbShard) FlushState(blockStart time.Time) fileOpState {
 	s.flushState.RUnlock()
 	return state
 }
-
-//HERE make this maybe?
-// func (s *dbShard) FlushStateWithLock(blockStart time.Time) fileOpState {
 
 func (s *dbShard) markFlushStateSuccessOrError(blockStart time.Time, version int, err error) error {
 	// Track flush state for block state
